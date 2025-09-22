1d51600b53aab2dddd8f4d4ca63368210835e492
damiodutola <todanye123@gmail.com>
2025-09-22 12:08:16 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-dev-tf-state-dami2-bucket"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table"
+    dynamodb_table = "my-dynamodb-table"
   }
 }
