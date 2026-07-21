f84d1384447dc9669fa2e449e135f38db4da97e1
damiodutola <todanye123@gmail.com>
2026-07-21 11:15:35 +0100

    Update backend.tf
    updated the name of the S3 bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-dev-tf-state-dami2-bucket"
+    bucket = "my-dev-tf-state-bucket-2026"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
